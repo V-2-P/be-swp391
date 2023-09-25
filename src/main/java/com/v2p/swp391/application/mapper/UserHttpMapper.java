0f52bae950b39b448655ff6993ad6e3c375c4566
tranthihongphuc <hongphuc0987@gmail.com>
2023-09-26 01:14:47 +0700

    feature/fix_category01-fix_mapper

--- src/main/java/com/v2p/swp391/application/mapper/UserHttpMapper.java
+++ src/main/java/com/v2p/swp391/application/mapper/UserHttpMapper.java
@@ -1,8 +1,6 @@
 package com.v2p.swp391.application.mapper;
 
-import com.v2p.swp391.application.model.Category;
 import com.v2p.swp391.application.model.User;
-import com.v2p.swp391.application.request.CategoryRequest;
 import com.v2p.swp391.application.request.UserRequest;
 import com.v2p.swp391.application.response.UserResponse;
 import org.mapstruct.Mapper;
