84360e423832dfe9f03f53aa9423035f972400db
tranthihongphuc <hongphuc0987@gmail.com>
2023-09-29 21:42:46 +0700

    feature/bird-01

--- src/main/java/com/v2p/swp391/common/constant/Image.java
+++ src/main/java/com/v2p/swp391/common/constant/Image.java
@@ -5,6 +5,6 @@ import com.v2p.swp391.application.model.AuthProvider;
 public class Image {
     public static final AuthProvider DEFAULT_AUTH_PROVIDER = AuthProvider.local;
     public static final String USER_IMAGE_PATH = "/images/users/";
-    public static final String BIRD_IMAGE_PATH = "uploads/images/birds/";
+    public static final String BIRD_IMAGE_PATH = "src/main/resources/uploads/images/birds";
     public static final String DEFAULT_AVATAR = "default.jpg";
 }
