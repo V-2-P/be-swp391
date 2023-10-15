bb2ff5edff03e8c50252738d39e1d61612281065
tranthihongphuc <hongphuc0987@gmail.com>
2023-10-15 21:24:13 +0700

    feature/voucher01

--- src/main/java/com/v2p/swp391/application/repository/VoucherRepository.java
+++ src/main/java/com/v2p/swp391/application/repository/VoucherRepository.java
@@ -2,12 +2,10 @@ package com.v2p.swp391.application.repository;
 
 import com.v2p.swp391.application.model.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface VoucherRepository extends JpaRepository<Voucher, Long > {
-    @Query("SELECT CASE WHEN COUNT(v) > 0 THEN TRUE ELSE FALSE END FROM Voucher v WHERE v.code = :code")
-    boolean existsByCode(@Param("code") String code);
+    boolean existsByCode(String code);
 
 
 }
