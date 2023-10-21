5b8452dd572bd7f10ea041d4579d6a9f487f7c84
Ynnub <vuthase172485@fpt.edu.vn>
2023-10-21 05:05:04 +0100

    update upload

--- src/main/java/com/v2p/swp391/common/constant/Image.java
+++ src/main/java/com/v2p/swp391/common/constant/Image.java
@@ -3,6 +3,6 @@ package com.v2p.swp391.common.constant;
 
 public class Image {
     public static final String USER_IMAGE_PATH = "/images/users/";
-    public static final String BIRD_IMAGE_PATH = "src/main/resources/uploads/images/birds";
+    public static final String BIRD_IMAGE_PATH = System.getProperty("user.dir") + "/uploads/birds";
     public static final String DEFAULT_AVATAR = "default.jpg";
 }
